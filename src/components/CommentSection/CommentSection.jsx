1a1a2a9a2885c82843f5c24c2474f4c5c5a7d982
Vladyslav Tishchenko <fsknnn@gmail.com>
2022-06-08 00:20:20 +0300

    Replace library buttons by buttons from component in CommentSection component.

--- src/components/CommentSection/CommentSection.jsx
+++ src/components/CommentSection/CommentSection.jsx
@@ -4,7 +4,8 @@ import { baseURL } from '../../urls';
 import { useContext, useState } from 'react';
 import PostsContext from '../../context/posts/postsContext';
 import Loader from '../UI/Loader/Loader';
-import { Button } from 'antd';
+// import { Button } from 'antd';
+import Button from '../UI//Button/Button';
 import Input from '../UI/Input/Input';
 import Comment from '../Comment/Comment';
 import UserContext from '../../context/user/userContext';
@@ -147,7 +148,7 @@ const CommentSection = ({ comments, setComments, getComments }) => {
           <h3>Write a comment</h3>
           {renderInputs()}
           <Button
-            type='primary'
+            type='primary-filled'
             onClick={writeCommentHandler}
             disabled={!isFormValid}
           >

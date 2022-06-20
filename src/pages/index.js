b4abe5e4df38386e28e4e7cfb65d29d068d8ad7d
Vladyslav Tishchenko <fsknnn@gmail.com>
2022-06-20 09:20:51 +0300

    Add index file with imports and exports for pages folder.

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import SignIn from './Auth/SignIn/SignIn';
+import SignUp from './Auth/SignUp/SignUp';
+import BlogList from './BlogList/BlogList';
+import CreatePost from './CreatePost/CreatePost';
+import Post from './Post/Post';
+import Profile from './Profile/Profile';
+
+export { SignIn, SignUp, BlogList, CreatePost, Post, Profile };

20e2a0556a5c59e847e9d6c1447276f05c583f52
Vladyslav Tishchenko <fsknnn@gmail.com>
2022-06-22 17:02:55 +0300

    Switch urls.

--- src/urls.js
+++ src/urls.js
@@ -1,10 +1,10 @@
-export const baseURL = 'http://localhost:8000/664';
-export const registerURL = 'http://localhost:8000/register';
-export const loginURL = 'http://localhost:8000/login';
+// export const baseURL = 'http://localhost:8000/664';
+// export const registerURL = 'http://localhost:8000/register';
+// export const loginURL = 'http://localhost:8000/login';
 
 // -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
 
-// export const baseURL = 'https://ekreative-json-server.herokuapp.com/664';
-// export const registerURL =
-//   'https://ekreative-json-server.herokuapp.com/register';
-// export const loginURL = 'https://ekreative-json-server.herokuapp.com/login';
+export const baseURL = 'https://ekreative-json-server.herokuapp.com/664';
+export const registerURL =
+  'https://ekreative-json-server.herokuapp.com/register';
+export const loginURL = 'https://ekreative-json-server.herokuapp.com/login';
